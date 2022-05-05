3efa2f3850b13b5f0952a917fd5fa474421fe513
Arbaaz-Zakir <z.arbaaz786@gmail.com>
2022-05-05 15:11:03 +0100

    added DELETE user functionality

--- restful-web-services/src/main/java/com/arbaaz/rest/restfulwebservices/user/UserController.java
+++ restful-web-services/src/main/java/com/arbaaz/rest/restfulwebservices/user/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,4 +46,14 @@ public class UserController {
 		
 		return ResponseEntity.created(location).build();
 	}
+	
+	//retrieve a user
+		@DeleteMapping("/users/{id}")
+		public void deleteUser(@PathVariable int id){
+			User user = service.deleteUser(id);
+			if(user==null) {
+				throw new UserNotFoundException("id: " + id);
+			}
+			
+		}
 }
