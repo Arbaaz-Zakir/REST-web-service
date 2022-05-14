dc6adcea19c502011f9b92f00854a704967df9b7
Arbaaz-Zakir <z.arbaaz786@gmail.com>
2022-05-14 18:27:26 +0100

    seperated classes into different packages

---
+++ users/src/main/java/com/arbaaz/rest/restfulwebservices/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arbaaz.rest.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ users/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, sysdate(), 'Andy');
+insert into user values(102, sysdate(), 'Jade');
+insert into user values(103, sysdate(), 'Wanda');
