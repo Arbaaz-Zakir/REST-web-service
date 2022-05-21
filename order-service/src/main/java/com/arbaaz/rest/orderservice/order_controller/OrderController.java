6cb3003d0c1a51fd992ed1e877c2bb73159fe099
Arbaaz-Zakir <z.arbaaz786@gmail.com>
2022-05-21 22:56:17 +0100

    started work on orderController added repository for orders

--- order-service/src/main/java/com/arbaaz/rest/orderservice/order_controller/OrderController.java
+++ order-service/src/main/java/com/arbaaz/rest/orderservice/order_controller/OrderController.java
@@ -1,9 +1,11 @@
 package com.arbaaz.rest.orderservice.order_controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.arbaaz.rest.orderservice.order_repository.OrderRepository;
 
+@RestController
 public class OrderController {
 	
 	@Autowired
