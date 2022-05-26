697560b295238f0a56688f79c5adb5906ff4d264
Arbaaz-Zakir <z.arbaaz786@gmail.com>
2022-05-26 15:53:07 +0100

    enabled feign clients

--- users/src/main/java/com/arbaaz/rest/restfulwebservices/UserServiceApplication.java
+++ users/src/main/java/com/arbaaz/rest/restfulwebservices/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableFeignClients
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
