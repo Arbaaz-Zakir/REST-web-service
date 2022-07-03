add425cfc7f38d2031bcf3e680e32d69d73de59f
Arbaaz-Zakir <z.arbaaz786@gmail.com>
2022-07-03 20:42:09 +0100

    created cloud-gateway to route all requests through and edits to other projects

---
+++ cloud-gateway/src/test/java/com/arbaaz/rest/restfulwebservices/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbaaz.rest.restfulwebservices.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
